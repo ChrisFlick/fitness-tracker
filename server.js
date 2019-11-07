2695344de5d09aa875de9fe483b36c6c45892f2e
Chris Flick <chrisflick@gmail.com>
2019-11-07 13:54:08 -0800

    ..'

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 var uristring =
     process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
-    'mongodb://localhost/workout';
+    'mongodb://heroku_q2lcnk95:password1@ds141218.mlab.com:41218/heroku_q2lcnk95';
 
 app.use(logger("dev"));
 
